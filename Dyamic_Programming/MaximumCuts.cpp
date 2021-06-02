0fe77bdac621d2c4995b2de86f613be1edfa95bf
Saksham Sharma <saksham.adi@gmail.com>
2021-06-02 11:56:44 +0530

    -maximum cuts DP

--- Dyamic_Programming/MaximumCuts.cpp
+++ Dyamic_Programming/MaximumCuts.cpp
@@ -13,12 +13,30 @@ int maxCut(int rod,int a,int b,int c)
     if(mx==-1) return mx;
     return 1+mx;
 }
+/*
+    Dynamic Programming Solution 
+    Bottom up manner used/ Tabulation
+*/
+int DP(int rod,int a,int b,int c)
+{
+    int dp[rod+1];
+    dp[0]=0;
+    for(int i=1;i<=rod;i++)
+    {
+        dp[i]=-1;
+        if(i-a>=0) dp[i] =max(dp[i],dp[i-a]);
+        if(i-b>=0) dp[i] =max(dp[i],dp[i-b]);
+        if(i-c>=0) dp[i] =max(dp[i],dp[i-c]);
+        if(dp[i]!=-1) dp[i]++;
+    }
+    return dp[rod];
+}
 void solve()
 {
     int rod;
     int a,b,c;
     cin>>rod>>a>>b>>c;
-    int res=maxCut(rod,a,b,c);
+    int res=DP(rod,a,b,c);
     cout<<res<<endl;
 }
 int main()
